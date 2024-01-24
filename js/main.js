154d926bae7796d30d25b61f2b2723f0062210c5
Nurzhan Myrzatayev <klemptan@gmail.com>
2024-01-24 20:19:59 +0600

    вернул скываемость блока

--- js/main.js
+++ js/main.js
@@ -8,20 +8,20 @@ function HideBanner() {
     banner.classList.add('hide');
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     let firstScroll = true;
+document.addEventListener('DOMContentLoaded', () => {
+    let firstScroll = true;
 
-//     const topBanner = document.querySelector('.top_banner');
-//     window.addEventListener('scroll', function () {
-//         const scrollHeight = window.scrollY;
-//         if (scrollHeight >= topBanner.offsetTop + topBanner.offsetHeight) {
-//             if (firstScroll) {
-//                 if (!localStorage.getItem('popup_banner_showed')) {
-//                     ShowBanner();
-//                     this.localStorage.setItem('popup_banner_showed', true);
-//                 }
-//                 firstScroll = false
-//             }
-//         }
-//     });
-// });
+    const topBanner = document.querySelector('.top_banner');
+    window.addEventListener('scroll', function () {
+        const scrollHeight = window.scrollY;
+        if (scrollHeight >= topBanner.offsetTop + topBanner.offsetHeight) {
+            if (firstScroll) {
+                if (!localStorage.getItem('popup_banner_showed')) {
+                    ShowBanner();
+                    this.localStorage.setItem('popup_banner_showed', true);
+                }
+                firstScroll = false
+            }
+        }
+    });
+});
